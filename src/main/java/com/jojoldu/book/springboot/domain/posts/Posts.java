177dfdcb64279dcbb04178b8a891a8a56fa83054
youdongkwan <dkyou7@naver.com>
2020-01-20 16:36:48 +0900

    3.2 spring data jpa 적용중

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,19 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
