89cb0886510c93c9e041e5ef495b2441fb47a63b
youdongkwan <dkyou7@naver.com>
2020-01-28 11:43:26 +0900

    5.3 로그인 시큐리티 구현 완료

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,15 +1,21 @@
 package com.jojoldu.book.springboot.config.auth;
 
+import com.jojoldu.book.springboot.config.auth.dto.OAuthAttributes;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.domain.user.User;
 import com.jojoldu.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +33,24 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         String registerationId = userRequest.getClientRegistration().getRegistrationId();
         // OAuth2 로그인 진행 시 키가 되는 필드값을 이야기한다.
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
-        OauthAttributes
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스입니다.
+        // 이후 네이버 등 다른 소셜 로그인도 이 클래스를 사용한다.
+        OAuthAttributes attributes = OAuthAttributes.of(registerationId,userNameAttributeName,oAuth2User.getAttributes());
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user",new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(
+                            new SimpleGrantedAuthority(
+                                    user.getRoleKey())),
+                                    attributes.getAttributes(),
+                                    attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                                                .map(entity->entity.update(attributes.getName(),attributes.getPicture()))
+                                                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
